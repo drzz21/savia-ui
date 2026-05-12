79422fc61cc19ba376fbbbca80b11e745c077082
Francisco Diosdado <drz.y2k@gmail.com>
2026-05-11 22:33:19 -0600

    chore

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts">
-import Button from './components/Button.vue'
+import Button from './components/Button.vue';
 </script>
 
 <template>
-  <Button label="Click me" variant="tertiary" size="small" type="text"/>
+	<Button
+		label="Click me"
+		variant="primary"
+		size="small"
+		type="default"
+		@click="console.log('1')"
+	/>
+  <Button
+		label="Click me"
+		variant="secondary"
+		size="small"
+		type="default"
+		@click="console.log('1')"
+	/>
 </template>
+

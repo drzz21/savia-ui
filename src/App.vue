f09f099fea3d452d02708612ce5dbd63551ca74c
Francisco Diosdado <drz.y2k@gmail.com>
2026-08-12 20:50:52 -0600

    chore: new version

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch, computed, nextTick } from 'vue';
+import { ref, watch, nextTick } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();

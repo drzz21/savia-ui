e95b9d7e224c1b1602cdb0b868081362ac253b2a
Francisco Diosdado <drz.y2k@gmail.com>
2026-08-14 11:55:49 -0600

    chore: added radio

---
+++ src/pages/RadioPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import BasicPattern from '../patterns/radio/BasicPattern.vue';
+import SizesPattern from '../patterns/radio/SizesPattern.vue';
+</script>
+
+<template>
+  <div class="space-y-16">
+    <h2 class="font-display text-3xl font-extrabold tracking-tight text-mist">Radio</h2>
+
+    <div id="basic"><BasicPattern /></div>
+    <hr class="border-leaf-muted/30 my-4" />
+    <div id="sizes"><SizesPattern /></div>
+  </div>
+</template>
